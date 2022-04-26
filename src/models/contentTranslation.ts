24ee3b8991338c8f07336352c28b8343e25ce7fe
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-04-26 02:38:40 -0300

    feat: Created Content Translation Model file

---
+++ src/models/contentTranslation.ts
@@ -0,0 +1,13 @@
+type ContentTranslation = {
+  presentationText: string
+  viewOnGooglePlayStore: string
+  viewSourceCode: string
+  developmentText: string
+  descriptionTitle: string
+  descriptionText: string
+  screenshotText: string
+  motivationTitle: string
+  motivationText: string
+}
+
+export default ContentTranslation
