1e298ffce56d0c1ddd650e39cee0544004cd6da8
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-04-26 06:12:26 -0300

    feat: Added SEO Translation to head tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
+import { getSeoTranslationForUserLocale } from '@services/pageTranslation'
 
 const HomePage: NextPage = () => {
+  const { locale } = useRouter()
+  const seoTranslation = getSeoTranslationForUserLocale(locale, 'home')
+
   return (
     <main className="flex flex-col w-full">
       <Head>
         <title>Felip&apos;s Apps</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content={seoTranslation.page.head.description}
+        />
+        <meta
+          name="keywords"
+          content={`${seoTranslation.page.head.keywords}`}
+        />
       </Head>
     </main>
   )
