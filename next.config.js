b8354e920c70b5cd63c83e4a239c6802fdf8b405
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-04-26 01:27:28 -0300

    feat: Added locales to Next configuration file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    localeDetection: true,
+    defaultLocale: 'en-US',
+    locales: ['en-US', 'pt-BR']
+  }
 }
 
 module.exports = nextConfig
