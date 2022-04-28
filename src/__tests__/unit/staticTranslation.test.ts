0b7fb7320bc02799d1e75649ebcc39f81c81df52
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-04-28 17:56:39 -0300

    test: Created a unit test file for the getStaticTranslationForUserLocale method

---
+++ src/__tests__/unit/staticTranslation.test.ts
@@ -0,0 +1,31 @@
+import { getStaticTranslationForUserLocale } from '@services/pageTranslation'
+import type { StaticTranslation } from '@models/index'
+import { locales } from '@utils/constants'
+
+describe('Testing "getStaticTranslationForUserLocale" method from "pageTranslation" module', () => {
+  test('Should check for static translation files for all available locales', () => {
+    const staticTranslations: StaticTranslation[] = []
+
+    locales.forEach(locale => {
+      staticTranslations.push(getStaticTranslationForUserLocale(locale))
+    })
+
+    staticTranslations.forEach(staticTranslation => {
+      const {
+        page: { body }
+      } = staticTranslation
+      const { titles } = body
+
+      expect(body.searchInputPlaceholder).toBeDefined()
+      expect(titles.goToHomePage).toBeDefined()
+      expect(titles.clearSearchInput).toBeDefined()
+      expect(titles.linkedInProfile).toBeDefined()
+      expect(titles.googlePlayStoreProfile).toBeDefined()
+      expect(titles.gitHubProfile).toBeDefined()
+      expect(titles.viewSourceCode).toBeDefined()
+      expect(titles.sendEmail).toBeDefined()
+      expect(titles.donations.payPal).toBeDefined()
+      expect(titles.donations.buyMeACoffee).toBeDefined()
+    })
+  })
+})
