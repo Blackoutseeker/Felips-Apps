ad569902ce59ce5d8ec32a304d36b5f41ada4c98
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-04-28 17:17:25 -0300

    feat: Added environment variables to Next configuration file

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
     localeDetection: true,
     defaultLocale: 'en-US',
     locales: ['en-US', 'pt-BR']
+  },
+  env: {
+    LINKEDIN_PROFILE_URL: process.env.LINKEDIN_PROFILE_URL,
+    GOOGLE_PLAY_PROFILE_URL: process.env.GOOGLE_PLAY_PROFILE_URL,
+    GITHUB_PROFILE_URL: process.env.GITHUB_PROFILE_URL
   }
 }
 
