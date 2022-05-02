a0e87720b58e5823c7da745948f64f3e691a4abd
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-02 16:06:29 -0300

    feat: Created SourceCodeButton component

---
+++ src/components/SourceCodeButton/index.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react'
+import { useRouter } from 'next/router'
+import { getStaticTranslationForUserLocale } from '@services/pageTranslation'
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa'
+import Styles from './SourceCodeButton.module.css'
+
+export const SourceCodeButton: FC = () => {
+  const { locale } = useRouter()
+  const staticTranslation = getStaticTranslationForUserLocale(locale)
+  const {
+    page: {
+      body: { titles }
+    }
+  } = staticTranslation
+
+  return (
+    <a
+      className={Styles.buttonWrapper}
+      href="https://github.com/Blackoutseeker/Felips-Apps"
+      target="_blank"
+      rel="noreferrer"
+      title={titles.viewSourceCode}
+    >
+      <button className={Styles.sourceCodeButton}>
+        <FaGithub color="#fff" size={30} />
+        <span className={Styles.buttonText}>{titles.viewSourceCode}</span>
+        <FaExternalLinkAlt color="#fff" size={20} />
+      </button>
+    </a>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './Header'
 export * from './Drawer'
 export * from './SearchBar'
+export * from './SourceCodeButton'
