b6a8590fe121ec3e8ebb70288a2a23c63f076009
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-02 20:02:41 -0300

    test: Created a unit test to check if all apps are on keywords in SEO translation files

--- src/__tests__/unit/seoTranslation.test.ts
+++ src/__tests__/unit/seoTranslation.test.ts
@@ -1,6 +1,6 @@
 import { getSeoTranslationForUserLocale } from '@services/pageTranslation'
 import type { SeoTranslation } from '@models/index'
-import { locales, pages } from '@utils/constants'
+import { locales, pages, apps } from '@utils/constants'
 
 describe('Testing "getSeoTranslationForUserLocale" method from "pageTranslation" module', () => {
   test('Should check if all the pages have an SEO translation file', () => {
@@ -15,6 +15,31 @@ describe('Testing "getSeoTranslationForUserLocale" method from "pageTranslation"
     seoTranslations.forEach(seoTranslation => {
       expect(seoTranslation.page.head.description).toBeDefined()
       expect(seoTranslation.page.head.keywords.length).toBeGreaterThan(0)
+    })
+  })
+
+  test('Should check if all SEO translation files have all apps as keywords', () => {
+    const seoTranslations: SeoTranslation[] = []
+
+    pages.forEach(page => {
+      locales.forEach(locale => {
+        seoTranslations.push(getSeoTranslationForUserLocale(locale, page))
+      })
+    })
+
+    const getKeywordsInLowerCase = (
+      seoTranslation: SeoTranslation
+    ): string[] => {
+      return seoTranslation.page.head.keywords.map(keyword =>
+        keyword.toLowerCase()
+      )
+    }
+
+    seoTranslations.forEach(seoTranslation => {
+      const keywords = getKeywordsInLowerCase(seoTranslation)
+      apps.forEach(app => {
+        expect(keywords.includes(app.toLowerCase())).toBeTruthy()
+      })
     })
   })
 })
