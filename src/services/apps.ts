c1550721d39c9f53eea2f7ba893f835138665aa2
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-06 23:34:32 -0300

    feat: Created getApps method with Lineker app

---
+++ src/services/apps.ts
@@ -0,0 +1,20 @@
+import { App } from '@models/index'
+import { getContentTranslationForUserLocale } from '@services/pageTranslation'
+import LinekerLogo from '@assets/images/apps/Lineker.png'
+
+/**
+ * Get all apps available for the user.
+ * @param {string | undefined} locale - The user's current `locale` (example: `en-US`).
+ * @returns {App[]} An array of apps for the given locale.
+ */
+
+export const getApps = (locale: string | undefined): App[] => [
+  {
+    name: 'Lineker',
+    image: LinekerLogo,
+    sourceCodeUrl: 'https://github.com/Blackoutseeker/Lineker-Mobile-Flutter',
+    googlePlayUrl:
+      'https://play.google.com/store/apps/details?id=com.FelipsTudio.lineker',
+    content: getContentTranslationForUserLocale(locale, 'Lineker')
+  }
+]
