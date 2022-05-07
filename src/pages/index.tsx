00301c5005caa939ca1cb3e9d83a0e14d7f35578
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-07 00:00:28 -0300

    feat: Created renderAppCards method to list AppCard components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { getSeoTranslationForUserLocale } from '@services/pageTranslation'
+import { getApps } from '@services/apps'
 import Head from 'next/head'
-import { Header, Footer } from '@components/index'
+import { Header, AppCard, Footer } from '@components/index'
 import Styles from '@styles/Page.module.css'
 
 const HomePage: NextPage = () => {
   const { locale } = useRouter()
   const seoTranslation = getSeoTranslationForUserLocale(locale, 'home')
+
+  const apps = getApps(locale)
+
+  const renderAppCards = () =>
+    apps.map(app => <AppCard key={app.name} app={app} />)
 
   return (
     <main className={Styles.pageContainer}>
@@ -25,7 +31,7 @@ const HomePage: NextPage = () => {
       </Head>
       <Header />
       <div className={Styles.appsListWrapper}>
-        <ul className={Styles.appsList}></ul>
+        <ul className={Styles.appsList}>{renderAppCards()}</ul>
       </div>
       <Footer />
     </main>
