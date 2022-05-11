0b0cd1d0b41df4f2e53ce1437875fc9fd514617f
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-10 23:26:07 -0300

    refactor: Added favicon to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
