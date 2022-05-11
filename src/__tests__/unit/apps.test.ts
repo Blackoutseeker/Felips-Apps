f32f41386224b84884cf1a2a6e98a2b5ba7338da
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-10 23:04:06 -0300

    test: Created a unit test to getApp method

--- src/__tests__/unit/apps.test.ts
+++ src/__tests__/unit/apps.test.ts
@@ -1,13 +1,29 @@
 import { apps } from '@utils/constants'
-import { getApps } from '@services/apps'
+import { getApps, getApp } from '@services/apps'
+
+const defaultLocale: string = 'en-US'
 
 describe('Testing "getApps" method from "apps" module', () => {
-  const defaultLocale: string = 'en-US'
-
   test('Should check if all apps match the apps names', () => {
     const appNames: string[] = getApps(defaultLocale).map(app => app.name)
     apps.forEach(app => {
       expect(appNames).toContain(app)
     })
+  })
+})
+
+describe('Testing "getApp" method from "apps" module', () => {
+  test('Should check if it correctly returns an existing app', () => {
+    const existingApps = apps
+    existingApps.forEach(existingApp => {
+      const app = getApp(existingApp, defaultLocale)
+      expect(app).toBeDefined()
+    })
+  })
+
+  test('Should check if it returns "undefined" for a non-existing app', () => {
+    const nonExistingApp = 'Non-existing App'
+    const app = getApp(nonExistingApp, defaultLocale)
+    expect(app).toBeUndefined()
   })
 })
