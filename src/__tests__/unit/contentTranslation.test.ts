6a03a500b301b4139e42d3915ff048cf1ec1fe57
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-12 15:29:55 -0300

    test: Added a unit test to verify if app content descriptions match SEO descriptions

--- src/__tests__/unit/contentTranslation.test.ts
+++ src/__tests__/unit/contentTranslation.test.ts
@@ -1,19 +1,20 @@
-import { getContentTranslationForUserLocale } from '@services/pageTranslation'
-import type { ContentTranslation } from '@models/index'
+import {
+  getContentTranslationForUserLocale,
+  getSeoTranslationForUserLocale
+} from '@services/pageTranslation'
+import type { ContentTranslation, SeoTranslation } from '@models/index'
 import { locales, apps } from '@utils/constants'
 
 describe('Testing "getContentTranslationForUserLocale" method from "pageTranslation" module', () => {
-  test('Should check if all the pages have an content translation file', () => {
-    const contentTranslations: ContentTranslation[] = []
+  const contentTranslations: ContentTranslation[] = []
 
-    apps.forEach(app => {
-      locales.forEach(locale => {
-        contentTranslations.push(
-          getContentTranslationForUserLocale(locale, app)
-        )
-      })
+  apps.forEach(app => {
+    locales.forEach(locale => {
+      contentTranslations.push(getContentTranslationForUserLocale(locale, app))
     })
+  })
 
+  test('Should check if all the pages have an content translation file', () => {
     contentTranslations.forEach(contentTranslation => {
       expect(contentTranslation.presentationText).toBeDefined()
       expect(contentTranslation.viewOnGooglePlayStore).toBeDefined()
@@ -24,6 +25,22 @@ describe('Testing "getContentTranslationForUserLocale" method from "pageTranslat
       expect(contentTranslation.screenshotText).toBeDefined()
       expect(contentTranslation.motivationTitle).toBeDefined()
       expect(contentTranslation.motivationText).toBeDefined()
+    })
+  })
+
+  test('Should check if the apps description matches the description of the SEO translations', () => {
+    const seoTranslations: SeoTranslation[] = []
+
+    apps.forEach(app => {
+      locales.forEach(locale => {
+        seoTranslations.push(getSeoTranslationForUserLocale(locale, app))
+      })
+    })
+
+    contentTranslations.forEach((contentTranslation, index: number) => {
+      expect(contentTranslation.descriptionText).toEqual(
+        seoTranslations[index].page.head.description
+      )
     })
   })
 })
