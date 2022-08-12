29029c855bc31c2c52b222db7fdb59584d9d113b
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-08-12 05:30:12 -0300

    refactor(_app.tsx): Removed Google AdSense script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,11 +8,6 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE_AD_SENSE_ID}`}
-          crossOrigin="anonymous"
-        />
       </Head>
       <Component {...pageProps} />
     </>
